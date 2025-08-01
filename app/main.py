ecd13a33d4430719533f01690b4c4897e4432956
Nora Basalamah <nbasalamah@sami.com.sa>
2025-08-01 10:38:25 +0300

    Implement API to communicate with UI.

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ import pytesseract
 from imutils.perspective import four_point_transform
 import numpy as np
 import pandas as pd
+import json
 
 import ollama
 
@@ -17,11 +18,13 @@ from app.ocr.ocr_parser import tensseract_receipt_parser
 from app.llama.llama_receipt_parser import ask_llava, ask_llama, encode_image_to_base64
 from app.forecast import arima_forecast, lstm_forecast, Prophet, sarimax_forecast
 from app.forecast import check_saving_target_yearly, check_saving_target, forecast_all_categories, forecast_category
+from app.offers.offers import get_matching_offers, get_offers
+from app.ather.ather_bot import ask_llama3
 import requests
 import base64
 
 app = FastAPI()
-
+history = ""
 
 @app.get("/")
 def read_root():
@@ -99,3 +102,32 @@ async def parse_receipt(file: UploadFile = File(...)):
 def read_items(file: UploadFile = File(...)):
     df = pd.read_csv(file.file)
     return {"Forcast" : "Prophet"}
+
+@app.post("/Offers/For_You_Offers")
+def get_general_offers():
+    offers_folder = os.path.join(os.path.dirname(__file__), "offers.json")
+    offers_folder = os.path.abspath(offers_folder)
+    purchases_folder = os.path.join(os.path.dirname(__file__), "purchases.json")
+    purchases_folder = os.path.abspath(purchases_folder)
+    with open(purchases_folder) as f:
+        purchase = json.load(f)
+    with open(offers_folder) as f:
+        offers = json.load(f) 
+        matched_offers = get_matching_offers(purchase, offers)
+        return matched_offers
+
+@app.post("/Offers/General_Offers")
+def get_general_offers():
+    folder = os.path.join(os.path.dirname(__file__), "offers.json")
+    folder = os.path.abspath(folder)
+    with open(folder) as f:
+        offers = json.load(f) 
+    matched_offers = get_offers(offers)
+    return matched_offers
+
+@app.post("/ather")
+def ask_ather(query: str):
+    global history
+    (ather, history) = ask_llama3(query, history)
+    print(ather)
+    return ather
--- app/offers/offers.py
+++ app/offers/offers.py
@@ -1,34 +1,22 @@
 import json
 from datetime import datetime
 
-with open("purchases.json") as f:
-    purchases = json.load(f)
-
-with open("offers.json") as f:
-    offers = json.load(f)    
-
 def get_matching_offers(purchases, offers, top_n=3):
     today = datetime.today().date()
+    products = []
     matches = []
-    items = []
     for p in purchases:
         for item in p["items"]:
             pid = item["name"]
             if int(item["quantity"]) > 2:
-                items.append(pid)
+                products.append(pid)
     for offer in offers:
-        if offer["name"] in items:
+        if offer["name"] in products:
             matches.append(offer)
     return matches
 
-def get_offers():
+def get_offers(offers):
     offerList = []
     for offer in offers:
         offerList.append(offer)
     return offerList
-
-if __name__ == "__main__":
-    matched_offers = get_matching_offers(purchases, offers)
-    general_offers  = get_offers()
-    print(f"\nMatched Offers: {matched_offers}")
-    print(f"\nGeneral Offers: {general_offers}")
